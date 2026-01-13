c9fd2fda9f604a369a7b3485b3dd9bdd0d3dd0aa
Jason Lee <jlee127@student.ubc.ca>
2026-01-13 10:26:19 -0800

    Create fifo_sequence.sv

---
+++ tb/fifo_sequence.sv
@@ -0,0 +1,10 @@
+class fifo_random_sequence extends uvm_sequence#(fifo_transaction);
+
+    `uvm_object_utils(fifo_base_sequence)
+
+    function new(string name = "fifo_base_sequence");
+        super.new(name);
+    endfunction
+
+endclass
+
