772c049ba95cc5fed984d65a9fa3e50fd8cb7b22
Jason Lee <jlee127@student.ubc.ca>
2026-01-13 16:27:33 -0800

    Refactor fifo_sequence classes to enhance structure and add detailed write sequence functionality

--- tb/fifo_sequence.sv
+++ tb/fifo_sequence.sv
@@ -1,4 +1,4 @@
-class fifo_random_sequence extends uvm_sequence#(fifo_transaction);
+class fifo_base_sequence extends uvm_sequence#(fifo_transaction);
 
     `uvm_object_utils(fifo_base_sequence)
 
@@ -6,5 +6,89 @@ class fifo_random_sequence extends uvm_sequence#(fifo_transaction);
         super.new(name);
     endfunction
 
+    // no body() for base sequence
+
 endclass
 
+// random operations on FIFO
+class fifo_random_sequence extends fifo_base_sequence;
+
+    `uvm_object_utils(fifo_random_sequence)
+
+    rand int num_trans;
+
+    constraint c_num_trans {
+        num_trans inside {[50:200]} //between 50 to 200 transactions
+    }
+
+    function new(string name = "fifo_random_sequence");
+        super.new(name);
+    endfunction
+
+    task body();
+        fifo_transaction req;
+        `uvm_info(get_type_name(), $sformatf("Starting random sequence with %0d transactions", num_trans), UVM_LOW)
+
+        repeat (num_trans) begin
+            //create transaction
+            req = fifo_transaction::type_id::create("req");
+
+            //wait driver to be ready and randomize it
+            start_item(req);
+
+            //randomize using transaction's constraints
+            if (!req.randomize()) begin
+                `uvm_error(get_type_name(), "Randomization failed!")
+            end
+
+            finish_item(req);
+
+            `uvm_info(get_type_name(), $sformatf("Sent: %s", req.convert2string()), UVM_HIGH)
+        end
+
+        `uvm_info(get_type_name(), "Random sequence completed", UVM_LOW)
+
+    endtask
+endclass
+
+// fill operations on FIFO
+class fifo_write_sequence extends fifo_base_sequence;
+
+    `uvm_object_utils(fifo_write_sequence)
+
+    rand int num_writes;
+
+    constraint c_num_writes {
+        num_writes inside {[10:20]};
+    }
+
+    function new(string name = "fifo_write_sequence");
+        super.new(name);
+    endfunction
+
+
+    task body();
+        fifo_transaction req;
+        
+        `uvm_info(get_type_name(), 
+                  $sformatf("Starting write sequence with %0d writes", num_writes), 
+                  UVM_LOW)
+        
+        repeat(num_writes) begin
+            req = fifo_transaction::type_id::create("req");
+            start_item(req);
+            
+            // Constrain to only writes
+            if (!req.randomize() with {
+                wr_en == 1;   // Always write
+                rd_en == 0;   // Never read
+            }) begin
+                `uvm_error(get_type_name(), "Randomization failed!")
+            end
+            
+            finish_item(req);
+        end
+        
+        `uvm_info(get_type_name(), "Write sequence completed", UVM_LOW)
+    endtask
+endclass
